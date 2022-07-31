f169ca6fe68ddde266da32080f91b3b3ac47be5a
rockstararyaman <93260229+rockstararyaman@users.noreply.github.com>
2022-07-31 10:54:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 //https://teachablemachine.withgoogle.com/models/eyHTVW94U/
 
 function startClassification() {
-    navigator.mediaDevices.getUserMedia({ audio: true});
+    navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
     classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/eyHTVW94U/model.json', modelReady);
 }
 
 function modelReady() {
     classifier.classify(gotResults);
 }
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+
+        document.getElementById("result_label").innerHTML = 'I can hear - ' + results[0].label;
+
+        img1 = document.getElementById('earing');
+
+        if (results[0].label == "Barking") {
+            img1.src = 'dog.png';
+        } else if (results[0].label == "Meowing") {
+            img1.src = 'cat.png'; 
+        }
+    }
+} 
